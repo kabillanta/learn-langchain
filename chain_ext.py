0adb1312e645393bfb67b9e18585e4211f2cd39b
Kabillan T A <kabillan1905@gmail.com>
2025-01-26 01:17:19 +0530

    langchain chains in parallel

--- chain_ext.py
+++ chain_ext.py
@@ -1,50 +1,20 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnableLambda ,RunnableParallel
+from langchain.schema.runnable import RunnableLambda
 model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp",api_key='AIzaSyA-WrW_nJnLdok50MkITXCdgmD7tcg78Pc')
 
-prompt_template = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are an expert product reviewer."),
-        ("human", "List the main features of the product {product_name}."),
-    ]
-)
+messages = [
+    ("system", "Give me the information in plain text without Markdown formatting . You are a {topic} expert"),
+    ("human", "give me {count} best commercial aircraft")
+]
+prompt_template = ChatPromptTemplate.from_messages(messages)
 
-def analyze_pros(features):
-    pros_template = ChatPromptTemplate.from_messages(
-        [
-            ("system", "You are an expert product reviewer."),
-            ("human","Given these features: {features}, list the pros of these features.",)
-        ]
-    ) 
-    return pros_template.format_prompt(features=features)
+upper_case = RunnableLambda(lambda x : x.upper())
+count_words = RunnableLambda(lambda x : f'Word Count is {len(x.split())}\n{x} ')
 
-def analyze_cons(features):
-    cons_template = ChatPromptTemplate.from_messages(
-        [
-            ("system", "You are an expert product reviewer. Be very concise ."),
-            ("human","Given these features: {features}, list the cons of these features.",)
-        ]
-    ) 
-    return cons_template.format_prompt(features=features)
 
-def combine_pros_cons(pros, cons):
-    return f"Pros:\n{pros}\n\nCons:\n{cons}"
+chain = prompt_template | model | StrOutputParser() |upper_case| count_words
 
-pros_branch = (
-    RunnableLambda(lambda x : analyze_pros(x)) | model | StrOutputParser() 
-)
-cons_branch = (RunnableLambda(lambda x : analyze_cons(x)) | model | StrOutputParser() 
-)
-
-chain = (
-    prompt_template 
-    | model 
-    | StrOutputParser() 
-    | RunnableParallel(branches={"pros":pros_branch,"cons":cons_branch}) 
-    | RunnableLambda(lambda x: combine_pros_cons(x["branches"]["pros"], x["branches"]["cons"]))
-)
-
-result = chain.invoke({"product_name":'Lenovo Legion Pro 5 16IRX9'})
+result = chain.invoke({"topic":'aircraft',"count":"3"})
 print(result)
